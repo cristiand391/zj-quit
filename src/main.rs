616fb35492f0e83afa744b1ce7a3deb7fe1924bd
Cristian Dominguez <6853656+cristiand391@users.noreply.github.com>
2024-05-28 01:41:32 -0300

    feat: prompt for confirm before killing session

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,37 @@
-use ansi_term::{Style, Colour::Fixed};
 use zellij_tile::prelude::*;
 
-use std::collections::{HashMap, BTreeMap};
+use std::collections::BTreeMap;
 
 #[derive(Default)]
 struct State {
-    mode_log: HashMap<String, usize>,
-    tabs: Vec<String>,
-    test_runs: usize,
-    userspace_configuration: BTreeMap<String, String>,
+    current_session_name: String,
 }
 
 register_plugin!(State);
 
 impl ZellijPlugin for State {
-    fn load(&mut self, configuration: BTreeMap<String, String>) {
-        self.userspace_configuration = configuration;
-        // we need the ReadApplicationState permission to receive the ModeUpdate and TabUpdate
-        // events
-        // we need the RunCommands permission to run "cargo test" in a floating window
-        request_permission(&[PermissionType::ReadApplicationState, PermissionType::RunCommands]);
-        subscribe(&[EventType::ModeUpdate, EventType::TabUpdate, EventType::Key]);
+    fn load(&mut self, _configuration: BTreeMap<String, String>) {
+        request_permission(&[
+            PermissionType::ReadApplicationState,
+            PermissionType::ChangeApplicationState,
+        ]);
+        subscribe(&[EventType::SessionUpdate, EventType::Key]);
     }
 
     fn update(&mut self, event: Event) -> bool {
         let mut should_render = false;
         match event {
-            Event::ModeUpdate(mode_info) => {
-                let mode = format!("{:?}", mode_info.mode);
-                let count = self.mode_log.entry(mode).or_insert(0);
-                *count += 1;
-                should_render = true;
-            }
-            Event::TabUpdate(tab_info) => {
-                self.tabs = tab_info.iter().map(|t| t.name.clone()).collect();
-                should_render = true;
+            Event::SessionUpdate(session_infos, _) => {
+                if let Some(current_session) = session_infos.iter().find(|s| s.is_current_session) {
+                    self.current_session_name = current_session.name.clone();
+                    should_render = true;
+                }
             }
             Event::Key(key) => {
-                if let Key::Char('n') = key {
-                    self.test_runs += 1;
-                    open_command_pane_floating(CommandToRun {
-                        path: "cargo".into(),
-                        args: vec!["test".to_owned()],
-                        cwd: None,
-                    });
+                if let Key::Char('\n') = key {
+                    kill_sessions(&[self.current_session_name.clone()])
+                } else if let Key::Esc = key {
+                    hide_self();
                 }
             }
             _ => (),
@@ -51,40 +39,14 @@ impl ZellijPlugin for State {
         should_render
     }
 
-    fn render(&mut self, rows: usize, cols: usize) {
-        let colored_rows = color_bold(CYAN, &rows.to_string());
-        let colored_cols = color_bold(CYAN, &cols.to_string());
-        println!("");
-        println!("I have {} rows and {} columns", colored_rows, colored_cols);
-        println!("");
-        println!("{} {:#?}", color_bold(GREEN, "I was started with the following user configuration:"), self.userspace_configuration);
-        println!("");
-        println!("{}", color_bold(GREEN, "Modes:"));
-        for (mode, count) in &self.mode_log {
-            let count = color_bold(ORANGE, &count.to_string());
-            println!("{} -> Changed {} times", mode, count);
-        }
-        println!("");
-        let current_tabs = color_bold(GREEN, "Current Tabs:");
-        let comma = color_bold(ORANGE, ", ");
-        println!("{} {}", current_tabs, self.tabs.join(&comma));
-        println!("");
-        if self.test_runs > 0 {
-            let test_run_count = color_bold(CYAN, &self.test_runs.to_string());
-            println!("Ran tests {} times!", test_run_count);
+    fn render(&mut self, _rows: usize, _cols: usize) {
+        if !self.current_session_name.is_empty() {
+            println!(
+                "Are you sure you want to kill the current session ({})?",
+                self.current_session_name
+            );
+            println!("Press Enter to confirm.");
+            println!("Press Esc to close this pane.");
         }
     }
-}
-
-pub const CYAN: u8 = 51;
-pub const GRAY_LIGHT: u8 = 238;
-pub const GRAY_DARK: u8 = 245;
-pub const WHITE: u8 = 15;
-pub const BLACK: u8 = 16;
-pub const RED: u8 = 124;
-pub const GREEN: u8 = 154;
-pub const ORANGE: u8 = 166;
-
-fn color_bold(color: u8, text: &str) -> String {
-    format!("{}", Style::new().fg(Fixed(color)).bold().paint(text))
 }
