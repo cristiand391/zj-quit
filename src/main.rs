a8ffead3a1ac2702b16d21d1be8eb14086b826d6
Cristian Dominguez <6853656+cristiand391@users.noreply.github.com>
2024-06-02 17:01:41 -0300

    fix: improve UI(#1)
    * fix: improve ui/use `quit_zellij` command
    
    improved UI:
      1. centered text
      2. added help text for the 2 available key actions
    
    plugin command:
    since only the current session may be killed let's use `quit_zellij` which doesn't require listening to session updates.
    
    * chore: add preCommit script/cargo lock update
    
    * Update README.md

--- src/main.rs
+++ src/main.rs
@@ -3,50 +3,55 @@ use zellij_tile::prelude::*;
 use std::collections::BTreeMap;
 
 #[derive(Default)]
-struct State {
-    current_session_name: String,
-}
+struct State {}
 
 register_plugin!(State);
 
 impl ZellijPlugin for State {
     fn load(&mut self, _configuration: BTreeMap<String, String>) {
-        request_permission(&[
-            PermissionType::ReadApplicationState,
-            PermissionType::ChangeApplicationState,
-        ]);
-        subscribe(&[EventType::SessionUpdate, EventType::Key]);
+        request_permission(&[PermissionType::ChangeApplicationState]);
+        subscribe(&[EventType::Key]);
     }
 
     fn update(&mut self, event: Event) -> bool {
-        let mut should_render = false;
         match event {
-            Event::SessionUpdate(session_infos, _) => {
-                if let Some(current_session) = session_infos.iter().find(|s| s.is_current_session) {
-                    self.current_session_name = current_session.name.clone();
-                    should_render = true;
-                }
-            }
             Event::Key(key) => {
                 if let Key::Char('\n') = key {
-                    kill_sessions(&[self.current_session_name.clone()])
+                    quit_zellij()
                 } else if let Key::Esc = key {
                     hide_self();
                 }
             }
             _ => (),
         };
-        should_render
+        false
     }
 
-    fn render(&mut self, _rows: usize, _cols: usize) {
-        if !self.current_session_name.is_empty() {
-            println!(
-                "Are you sure you want to kill the current session ({})?",
-                self.current_session_name
-            );
-            println!("Press Enter to confirm.");
-            println!("Press Esc to close this pane.");
-        }
+    fn render(&mut self, rows: usize, cols: usize) {
+        let confirmation_text = "Are you sure you want to kill the current session?".to_string();
+        let confirmation_y_location = (rows / 2) - 2;
+        let confirmation_x_location = cols.saturating_sub(confirmation_text.chars().count()) / 2;
+
+        print_text_with_coordinates(
+            Text::new(confirmation_text),
+            confirmation_x_location,
+            confirmation_y_location,
+            None,
+            None,
+        );
+
+        let help_text = "Help: <ENTER> - Confirm, <ESC> - Hide";
+        let help_text_y_location = rows - 1;
+        let help_text_x_location = cols.saturating_sub(help_text.chars().count()) / 2;
+
+        print_text_with_coordinates(
+            Text::new(help_text)
+                .color_range(3, 6..13)
+                .color_range(3, 25..30),
+            help_text_x_location,
+            help_text_y_location,
+            None,
+            None,
+        );
     }
 }
